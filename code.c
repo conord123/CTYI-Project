0456376401c25d8172f00688d1351870b97b86bf
conord123 <conor.delaney1@ctyi.org>
2019-07-08 14:06:45 +0100

    adding make and updating code.c

--- code.c
+++ code.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
-#include <sys/wait.h>
-#include <unistd.h>
+//#include <malloc.h>
+//#include <sys/wait.h>
+//#include <unistd.h>
+#include <math.h>
 
 void main(int argc, char *argv[])
 {
 
     printf ("booting up...\n");
 
-    printf ("enter command\n for list of commands enter 'help'\n>");
+    printf ("enter command\nfor list of commands enter 'help'\n");
 
     while (1)
     {
-        char opr[20], cmd[10], arg1[10], arg2[10];
+        char cmd[100], opr[10], arg1[10], arg2[10];
+        const char delim[2] = " ";
+        char *token;
         printf(">");
-        gets(opr);
-        char token = strtok(opr, " ");
+        fgets(cmd,100,stdin);
+        token = strtok(cmd, delim);
+        int i=0;
 
         while( token != NULL ) 
         {
-            int i=0;
-
             if(i==0) 
             {
-                strcpy(cmd, token);
+                strcpy(opr, token);
             } 
             else if(i==1) 
             {
@@ -36,14 +38,44 @@ void main(int argc, char *argv[])
                 strcpy(arg2, token);
             }
             i++;
-            token = strtok(NULL, " ");
+            token = strtok(NULL, delim);
         }
-        if(strcmp(opr , "add")==0)
+
+        if(strcmp(opr , "help")==0)
         {
-            int a;
-            a = atoi(argv[1]) + atoi(argv[2]);
-            printf("%d", a);
+            printf("add = adds of two numbers\nsub = subtracts the second number from the first\nmul = multiplies two numbers together\ndiv = divides the first number by the second\npow = raise the first number to the power of the second\n");
+        }
+        else if(strcmp(opr , "add")==0)
+        {
+            float a;
+            a = atoi(arg1) + atoi(arg2);
+            printf("%f\n", a);
+        }
+        else if(strcmp(opr , "sub")==0)
+        {
+            float a;
+            a = atoi(arg1) - atoi(arg2);
+            printf("%f\n", a);
+        }
+        else if(strcmp(opr , "div")==0)
+        {
+            float a;
+            a = (float) atoi(arg1)/atoi(arg2);
+            printf("%.2f\n", a);
+        }
+        else if(strcmp(opr , "mul")==0)
+        {
+            float a;
+            a = atoi(arg1)*atoi(arg2);
+            printf("%f\n", a);
+        }
+        else if(strcmp(opr , "pow")==0)
+        {
+            float a;
+        
+            printf("%d, %d \n", (int) atoi(arg1), (int) atoi(arg2));
+            a = pow(atoi(arg1), atoi(arg2));
+            printf("%f\n", a);
         }
     }
-    
 }
