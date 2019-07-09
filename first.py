6170607fbc85c717904a8147c1d6526bd2f06592
conord123 <conor.delaney1@ctyi.org>
2019-07-09 19:51:04 +0100

    fully functional x's and o's

--- first.py
+++ first.py
@@ -1,3 +1,6 @@
+import os
+os.system('cls')
+
 a = "#"
 b = "#"
 c = "#"
@@ -8,167 +11,141 @@ g = "#"
 h = "#"
 i = "#"
 
+owin = 0
 xwin = 0
-while(xwin == 0):
-    print ""+a,""+b,""+c,"| a b c\n"+d,""+e,""+f,"| d e f\n"+g,""+h,""+i,"| g h i"
-    xchoice = raw_input("Place your x: ")
+while(xwin == 0 and owin == 0):
     xplace = 0
     while(xplace == 0):
+        os.system('clear')
+        print ""+a,""+b,""+c,"| a b c\n"+d,""+e,""+f,"| d e f\n"+g,""+h,""+i,"| g h i"
+        xchoice = raw_input("Place your x: ")
         if(xchoice == 'a'):
-            if(a == 'o'):
-                print "invalid choice"
-            elif (a == 'x'):
+            if(a == 'o' or a == 'x'):
                 print "invalid choice"
             else:
                 a = 'x'
                 xplace = 1
         elif(xchoice == 'b'):
-            if(b == 'o'):
-                print "invalid choice"
-            elif(b == 'x'):
+            if(b == 'o' or b == 'x'):
                 print "invalid choice"
             else:
                 b = 'x'
                 xplace = 1
         elif(xchoice == 'c'):
-            if(c == 'o'):
-                print "invalid choice"
-            elif(c == 'x'):
+            if(c == 'o' or c == 'x'):
                 print "invalid choice"
             else:
                 c = 'x'
                 xplace = 1
         elif(xchoice == 'd'):
-            if(d == o):
-                print "invalid choice"
-            elif(d == x):  
+            if(d == 'o' or d == 'x'):
                 print "invalid choice"
             else:
-                d = x
+                d = 'x'
                 xplace = 1
         elif(xchoice == 'e'):
-            if(e == o):
-                print "invalid choice"
-            elif(e == x):
+            if(e == 'o' or e == 'x'):
                 print "invalid choice"
             else:
-                e = x
+                e = 'x'
                 xplace = 1
         elif(xchoice == 'f'):
-            if(f == o):
-                print "invalid choice"
-            elif(f == x):
+            if(f == 'o' or f == 'x'):
                 print "invalid choice"
             else:
-                f = x
+                f = 'x'
                 xplace = 1
         elif(xchoice == 'g'):
-            if(g == o):
-                print "invalid choice"
-            elif(g == x):
+            if(g == 'o' or g == 'x'):
                 print "invalid choice"
             else:
-                g = x
+                g = 'x'
                 xplace = 1
         elif(xchoice == 'h'):
-            if(h == o):
-                print "invalid choice"
-            elif(h == x):
+            if(h == 'o' or h == 'x'):
                 print "invalid choice"
             else:
-                h = x
+                h = 'x'
+                xplace = 1
         elif(xchoice == 'i'):
-            if(i == o):
-                print "invalid choice"
-            elif(i == x):
+            if(i == 'o' or i == 'x'):
                 print "invalid choice"
             else:
-                i = x
+                i = 'x'
                 xplace = 1
         else:
             print"invalid choice"
-    owin = 0
-    print ""+a,""+b,""+c,"| a b c\n"+d,""+e,""+f,"| d e f\n"+g,""+h,""+i,"| g h i"
-    ochoice = raw_input("Place your o: ") 
+        
+    if ((a=='x' and b=='x' and c=='x') or (d=='x' and e=='x' and f=='x') or (g=='x' and h=='x' and i=='x') or (a=='x' and d=='x' and g=='x') or (b=='x' and e=='x' and h=='x') or (c=='x' and f=='x' and i=='x') or (a=='x' and e=='x' and i=='x') or (c=='x' and e=='x' and g=='x')):
+        xwin = 1
+        os.system('clear')
+        print ""+a,""+b,""+c,"| a b c\n"+d,""+e,""+f,"| d e f\n"+g,""+h,""+i,"| g h i"
+        print "x wins"
+        break
     oplace = 0
     while(oplace == 0):
+        os.system('clear')
+        print ""+a,""+b,""+c,"| a b c\n"+d,""+e,""+f,"| d e f\n"+g,""+h,""+i,"| g h i"
+        ochoice = raw_input("Place your o: ")
         if(ochoice == 'a'):
-            if(a == 'x'):
-                print "invalid choice"
-            elif(a == 'o'):
+            if(a == 'x' or a == 'o'):
                 print "invalid choice"
             else:
                 a = 'o'
                 oplace = 1
         elif(ochoice == 'b'):
-            if(b == 'x'):
-                print "invalid choice"
-            elif(b == 'o'):
+            if(b == 'x' or b == 'o'):
                 print "invalid choice"
             else:
                 b = 'o'
                 oplace = 1
         elif(ochoice == 'c'):
-            if(c == x):
-                print "invalid choice"
-            elif(c == x):
+            if(c == 'x' or c == 'o'):
                 print "invalid choice"
             else:
-                c = o
+                c = 'o'
                 oplace = 1
         elif(ochoice == 'd'):
-            if(d == x):
-                print "invalid choice"
-            elif(d == x):
+            if(d == 'x' or d == 'o'):
                 print "invalid choice"
             else:
-                d = o
+                d = 'o'
                 oplace = 1
         elif(ochoice == 'e'):
-            if(e == x):
-                print "invalid choice"
-            elif(e == x):
+            if(e == 'x' or e == 'o'):
                 print "invalid choice"
             else:
-                e = o
+                e = 'o'
                 oplace = 1
         elif(ochoice == 'f'):
-            if(f == x):
-                print "invalid choice"
-            elif(f == x):
+            if(f == 'x' or f =='o'):
                 print "invalid choice"
             else:
-                f = o
+                f = 'o'
                 oplace = 1
         elif(ochoice == 'g'):
-            if(g == x):
-                print "invalid choice"
-            elif(g == x):
+            if(g == 'x' or g == 'o'):
                 print "invalid choice"
             else:
-                g = o
+                g = 'o'
                 oplace = 1
         elif(ochoice == 'h'):
-            if(h == x):
-                print "invalid choice"
-            elif(h == x):
+            if(h == 'x' or h == 'o'):
                 print "invalid choice"
             else:
-                h = o
+                h = 'o'
                 oplace = 1
         elif(ochoice == 'i'):
-            if(i == x):
-                print "invalid choice"
-            elif(i == x):
+            if(i == 'x' or i == 'o'):
                 print "invalid choice"
             else:
-                i = o
+                i = 'o'
                 oplace = 1
         else:
-            print"invalid choice"
-    if ((a and b and c) or (d and e and f) or (g and h and i) or (a and d and g) or b,e,h or c,f,i or a,e,i or c,e,g is 'x'):
-        xwin = 1
-        print "x wins" 
-    if (a,b,c or d,e,f or g,h,i or a,d,g or b,e,h or c,f,i or a,e,i or c,e,g is 'o'):
+            print"invalid choice" 
+    if ((a=='o' and b=='o' and c=='o') or (d=='o' and e=='o' and f=='o') or (g=='o' and h=='o' and i=='o') or (a=='o' and d=='o' and g=='o') or (b=='o' and e=='o' and h=='o') or (c=='o' and f=='o' and i=='o') or (a=='o' and e=='o' and i=='o') or (c=='o' and e=='o' and g=='o')):
         owin = 1
+        os.system('clear')
+        print ""+a,""+b,""+c,"| a b c\n"+d,""+e,""+f,"| d e f\n"+g,""+h,""+i,"| g h i"
         print "o wins" 
+        break
